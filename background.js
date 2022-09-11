6b0261d30fdec37fba63d38a99d5fafe9d16dea1
hyeezin <hyeeeezin@gmail.com>
2022-09-12 01:30:26 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -4,6 +4,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img"); //createElement로 html에 element를 추가한다. 
 
-bgImage.src = `../img/${chosenImage}`; //src를 이용하여 img폴더를 지정
+bgImage.src = `img/${chosenImage}`; //src를 이용하여 img폴더를 지정
 
 document.body.appendChild(bgImage); //appendChild(prepend)로 html에 이미지를 추가해준다.
